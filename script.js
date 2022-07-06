ca85a92323690840707b149ce7d7d33b45572521
Anderson Alves <nenoalveslopes@hotmail.com>
2022-07-06 20:19:38 -0300

    Nova funcionalidade

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("Olá Git");
+
+function handleClick(){
+   let h1 = document.querySelector('h1')
+   if(h1.style.color == '#0000ff'){
+    h1.style.color = '#ff0000';
+   }else {
+      h1.style.color = '#0000ff';
+   }
+}
